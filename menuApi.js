0072d1269c2efb47787289c3f641074a2624ee66
Rambabu <115921619+Rambabumca@users.noreply.github.com>
2023-01-30 11:02:48 +0530

    Add files via upload

---
+++ menuApi.js
@@ -0,0 +1,105 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "I love Maggi Julius Maggi experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoida",
+    category: "evening",
+    price: "20₹",
+    description:
+      "I love allu pakoda Maggi experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to. ",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "I love corn experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "I love chola experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "I love Pizza experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "I love Non-Veg Thali experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to. ",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "I love Sweets experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to. . ",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "I love Rajma Rice experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to. ",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "I love samaso experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+  {
+    id: 10,
+    image: "../images/samosa.jpg",
+    name: "Icecream",
+    category: "evening",
+    price: "10₹",
+    description:
+      "I love Icecream experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+  {
+    id: 11,
+    image: "../images/samosa.jpg",
+    name: "Icecream",
+    category: "lunch",
+    price: "10₹",
+    description:
+      "I love Icecream experimented for two years with different methods of mechanical and chemical processing of legumes and different mixtures. The results were presented to . ",
+  },
+
+];
+
+export default Menu;
